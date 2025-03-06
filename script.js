24edc87ac1582c3bd8a6b1681592585ce2058922
raul <raulbeldiman4@gmail.com>
2025-03-06 22:01:26 +0200

    test

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById('schimba-tema').addEventListener('click', function() {
+    if (document.body.classList.contains('dark-mode')) {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+    } else {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+    }
+});
+
+document.getElementById('varsta').addEventListener('click', function() {
+    alert('Varsta mea (Lionel Messi) este de 37 de ani.');
+});
+
+document.getElementById('detalii').addEventListener('click', function() {
+
+    document.getElementById('textdeschimbat').classList.toggle('text-buton-rosu');
+})
+
+document.getElementById('ascunde tabel').addEventListener('click', function() {
+    let tabel = document.querySelector('.tabel'); 
+    if (tabel.style.display === 'none') {
+        tabel.style.display = 'table'; 
+    } else {
+        tabel.style.display = 'none'; 
+    }
+});
